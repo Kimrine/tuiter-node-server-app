e468548b5acf2d0b19a1657f6b047dc8831cb7ff
kimrine <347642700@qq.com>
2022-11-16 22:29:14 -0800

    change tuits content

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,19 +1,36 @@
 export default [
     {
-     "_id": "123", "topic": "Space", "username": "SpaceX",
-     "handle": "@spacex", "time": "2h", "image": "spacex.png",
-     "title": "SpaceX's Mission",
-     "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-     "liked": true, "likes": 2345,
-     "replies": 123, "retuits": 432
+     "_id": "123", 
+     "postedBy": {
+        "username": "spacex"
+    },
+    "handle": "spacex",
+    "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+    "avatarImage": "https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg",
+    "liked": false,
+    "disliked": false,
+    "likes": 0,
+    "dislikes": 0,
+    "stats": {
+        "retuits": 0,
+        "comments": 0
+    }
     },
     {
-     "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-     "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-     "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+     "_id": "234",  
+     "postedBy": {
+        "username": "The Boring Company"
+    },
+     "handle": "boringcompany",
      "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-     "liked": true, "likes": 2345,
-     "replies": 123, "retuits": 432
+     "liked": true,
+     "disliked": false,
+     "likes": 246,
+     "dislikes": 0,
+     "stats": {
+         "retuits": 0,
+         "comments": 0
+     }
     },
     ];
     
