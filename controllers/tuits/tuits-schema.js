ad167b03130b3fd710479008b4664fa121054fc0
kimrine <347642700@qq.com>
2022-11-29 17:30:51 -0800

    modify tuits schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 const schema = mongoose.Schema({
     tuit: String,
+    postedBy: String,
+    handle: String,
+    avatarImage: String,
     likes: Number,
     liked: Boolean,
     dislikes: Number,
     disliked: Boolean,
+    comments: Number,
+    retuits: Number
 },{collection: 'tuits'});
 
 export default schema;
